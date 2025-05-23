08cb1631e122aef0168c84c39cf7cfe1898f262b
Rohit-singhal-crypto <rohityadavrohit8361@gmail.com>
2025-05-23 19:12:34 +0530

    swiper

---
+++ assects/Java/app.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000, // har 3 seconds mein slide change hoga
+    disableOnInteraction: false, // user interaction ke baad bhi autoplay chalu rahega
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    600: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1280: {
+      slidesPerView: 6,
+    },
+  },
+});
